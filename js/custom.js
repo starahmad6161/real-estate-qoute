d31878b03ff27a39dd8d849bf3793c0e91fcb2f8
starahmad6161 <starahmad616161@gmail.com>
2020-11-21 15:07:37 +0200

    second

--- js/custom.js
+++ js/custom.js
@@ -6,20 +6,22 @@ $(function () {
     let dataSRow;
 
 
-    $(".quote-section .first-row .box-item").on('click', function() {
-        $(".quote-section .second-row .box-item").removeClass('active');
-        $('.quote-section .inner-content .inner-box').hide();
-        $(this).addClass('active').siblings().removeClass('active');
+    $(".quote-quote-section .first-row .box-item img").on('click', function() {
+        $(".quote-quote-section .second-row .box-item").removeClass('active');
+
+        $('.quote-quote-section .inner-content .inner-box').hide();
+
+        $(this).parents('.box-item').addClass('active').siblings().removeClass('active');
         dataFRow = $(this).data('f-row');
-        $('.quote-section .second-row').addClass('active');
+        $('.quote-quote-section .second-row').addClass('active');
     });
 
     
-    $(".quote-section .second-row .box-item").on('click', function() {
-        $(this).addClass('active').siblings().removeClass('active');
+    $(".quote-quote-section .second-row .box-item img").on('click', function() {
+        $(this).parents('.box-item').addClass('active').siblings().removeClass('active');
         dataSRow = $(this).data('s-row');
 
-        $(this).parents('.rows-container').find(`.${dataFRow}${dataSRow}`).show().siblings().hide();
+        $(this).parents('.rows-container').find(`.${dataFRow}${dataSRow}`).show().siblings('.inner-box').hide();
         
     });
 });
